da8b5fbd922ed3b575f35d231f474f531e7e2ca6
Artem <artem.fatuev@gmail.com>
2025-05-11 15:14:49 +0300

    хз

--- data/users.py
+++ data/users.py
@@ -1,6 +1,9 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms.validators import DataRequired
 
 
 class User(SqlAlchemyBase):
@@ -19,3 +22,13 @@ class User(SqlAlchemyBase):
                                      default=datetime.datetime.now)
     role = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=False, nullable=False)
+
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    about = TextAreaField("Немного о себе")
+    submit = SubmitField('Войти')
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, abort, request
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+
 from data import db_session
+from data.users import User, RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,15 +12,39 @@ def main():
     db_session.global_init("db/Frilans.db")
     app.run()
 
-#@app.route("/")
-# def index():
-#     db_sess = db_session.create_session()
-#     jobs = db_sess.query(users).all()
-#     names = dict()
-#     for job in jobs:
-#         user = db_sess.query(User).filter(User.id == job.team_leader).first()
-#         names[job.team_leader] = user.surname + ' ' + user.name
-#     return render_template("index.html", jobs=jobs, names=names, title="Марсиане")
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(User).all()
+    names = dict()
+    for job in jobs:
+        user = db_sess.query(User).filter(User.id == job.team_leader).first()
+        names[job.team_leader] = user.surname + ' ' + user.name
+    return render_template("index.html", jobs=jobs, names=names, title="Марсиане")
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
